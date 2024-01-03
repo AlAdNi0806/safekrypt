8ab7124c6f5e15796f3fd53e8f7d44af08bf3e73
Alex <hetdro451@gmail.com>
2024-01-03 22:10:54 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+import './index.css'
+import { TransactionProvider } from './context/TransactionContext.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+ <React.StrictMode>
+   <TransactionProvider>
+     <App />
+   </TransactionProvider>
+ </React.StrictMode>
+);
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x0A6ac94385742Fb4f4DA229E9aAd7797134c1b59';
